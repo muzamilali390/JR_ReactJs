a0fdf9f9711bed7c95edff3ee552034866370ed7
Muzamilali2017355 <u2017355@giki.edu.pk>
2021-10-11 19:40:35 +0500

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+clone it from online repo or copy paste the folder in any directory 
+go to that directory run following  commands
+npm run install
+npm start
---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {  useState } from 'react';
+import './App.css';
+import { Button } from '@material-ui/core';
+import { TextField } from '@mui/material';
+
+const App = () =>{
+
+  const [number, setnumber] = useState(0)
+  const [output1, setoutput1] = useState(false)
+  const [output2, setoutput2] = useState(0)
+
+  async function IsPrime()
+  {
+    
+    if (number===1)
+    {
+      setoutput1(false);
+      setoutput2(number);
+    }
+    else if(number === 2)
+    {
+      setoutput1(true);
+      setoutput2(number);
+    }
+    else
+    {
+      for(var x = 2; x < number; x++)
+      {
+        if(number % x === 0)
+        {
+          setoutput1(false);
+          setoutput2(number);
+        }
+      }
+      setoutput1(true);
+      setoutput2(number);  
+    }
+  }
+
+  return (
+    <div>
+        <div>
+            <h1>Fun With Prime Numbers</h1>
+            <TextField id="input" 
+            label="Enter A Number " 
+            variant="outlined" 
+            onChange={(e) => setnumber(e.target.value)}
+            value={number}
+            />
+            <Button variant="contained" onClick={IsPrime}>Is it Prime ?</Button>
+            <h3>output 1: {output1}</h3>
+            <h3>output 2: {output2}</h3>
+        </div>
+    </div>
+  );
+}
+
+export default App;
